5d576956f01e669d7c7ed5c338c1da15f62310a8
sam-karis <samkaris75@gmail.com>
2019-08-13 01:05:33 +0300

    add login tests

--- airtech/tests/conftest.py
+++ airtech/tests/conftest.py
@@ -1,6 +1,22 @@
 import pytest
 from django.test import RequestFactory
+from django.urls import reverse
 from mixer.backend.django import mixer
+
+from airtech.apps.authentication.views import RegisterUserAPIView
+
+
+@pytest.fixture
+def factory():
+    yield RequestFactory()
+
+
+@pytest.fixture
+def urls():
+    yield {
+        'register_url': reverse('authentication:register_user'),
+        'login_url': reverse('authentication:login_user')
+    }
 
 
 @pytest.fixture
@@ -13,11 +29,28 @@ def user_data():
     }
 
 
-@pytest.fixture
-def factory():
-    yield RequestFactory()
-
-
 @pytest.fixture()
 def user():
-    yield mixer.blend('authentication.User', username='testuser')
+    yield mixer.blend(
+        'authentication.User', username='testuser',
+        email='test2@example.com', password='dummyPass1#'
+    )
+
+
+@pytest.fixture
+def user_login_data():
+    yield {'email': 'test@example.com', 'password': 'dummyPass1#'}
+
+
+@pytest.fixture
+def invalid_login_data():
+    yield {'email': 'test@example.com', 'password': 'wrongPass'}
+
+
+@pytest.fixture
+def register_user(factory, urls, user_data):
+    request = factory.post(
+        urls['register_url'], user_data,
+        content_type='application/json'
+    )
+    yield RegisterUserAPIView.as_view()(request)
