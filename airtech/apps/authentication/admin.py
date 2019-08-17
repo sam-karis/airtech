8f3e50f756a315f7fbde81ac01648722b3fdb746
sam-karis <samkaris75@gmail.com>
2019-08-17 21:40:46 +0300

    register profile model on dashboard add profile related urls

--- airtech/apps/authentication/admin.py
+++ airtech/apps/authentication/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from airtech.apps.authentication.models import User
+
+from airtech.apps.authentication.models import Profile, User
 
 admin.site.register(User)
+admin.site.register(Profile)
--- airtech/apps/authentication/urls.py
+++ airtech/apps/authentication/urls.py
@@ -1,11 +1,24 @@
+from django.conf.urls import url
 from django.urls import path
 
-from airtech.apps.authentication.views import (LoginUserAPIView,
-                                               RegisterUserAPIView)
+from airtech.apps.authentication.views import (DeleteProfileImageAPIView,
+                                               GetAllProfileAPIView,
+                                               LoginUserAPIView,
+                                               RegisterUserAPIView,
+                                               SpecificUserProfileAPIView)
 
 app_name = 'auth'
 
 urlpatterns = [
     path('register/', RegisterUserAPIView.as_view(), name='register_user'),
     path('login/', LoginUserAPIView.as_view(), name='login_user'),
+    path('users/profiles/', GetAllProfileAPIView.as_view(), name='profiles'),
+    url(
+        r'^users/profiles/(?P<username>[a-zA-Z0-9]+)$',
+        SpecificUserProfileAPIView.as_view(), name='user_profile'
+    ),
+    path(
+        'users/profiles/image/', DeleteProfileImageAPIView.as_view(),
+        name='delete_image'
+    ),
 ]
